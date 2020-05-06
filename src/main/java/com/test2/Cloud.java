17cb747b83023ce53830da70c0e56162f06a350c
ennix <ennix123@gmail.com>
2020-05-06 10:28:26 +0300

    first

---
+++ src/main/java/com/test2/Cloud.java
@@ -0,0 +1,17 @@
+package com.test2;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Cloud {
+    public static void main(String[] args) throws IOException {
+        Path path2 = Paths.get("C:\\Users\\Ennix\\Desktop\\data-data.csv");
+        List<String> strings = Files.readAllLines(path2);
+        System.out.print(strings);
+        
+        //strings.stream().map(s -> s.contains("адмиралтейский,пер.антоненко (от казанской ул. до наб.реки мойки)")).sorted().forEach(System.out::println);
+
+    }
+}

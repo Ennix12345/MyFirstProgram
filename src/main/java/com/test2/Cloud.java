1c93ab878eb3e88f661406a496bbc84d7bb0f61e
ennix <ennix123@gmail.com>
2020-05-06 13:43:06 +0300

    third

--- src/main/java/com/test2/Cloud.java
+++ src/main/java/com/test2/Cloud.java
@@ -11,6 +11,7 @@ public class Cloud {
         List<String> strings = Files.readAllLines(path2);
         System.out.print(strings);
         System.out.println("I have changed my commit");
+        System.out.println("--------------------");
         
         //strings.stream().map(s -> s.contains("адмиралтейский,пер.антоненко (от казанской ул. до наб.реки мойки)")).sorted().forEach(System.out::println);
 
